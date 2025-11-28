ac22d848c490f0cace4a00c5e6b14b48e7b44df2
ar.r.lysenko <ar.r.lysenko@tinkoff.ru>
2025-11-28 15:58:28 +0300

    BB-14 feat: add notification outbox service

--- src/main/resources/db/changelogs/0002-ICAL.sql
+++ src/main/resources/db/changelogs/0002-ICAL.sql
@@ -16,18 +16,25 @@ CREATE TABLE notification_outbox (
     payload JSONB NOT NULL,
     attempts_count int DEFAULT 0,
     next_retry_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT notification_outbox_pkey PRIMARY KEY (id)
 );
 
 --changeset quicklybly:BB-14-create-notification-outbox-status-idx
-CREATE INDEX notification_outbox_status_idx
-ON notification_outbox (status, created_at ASC)
-WHERE status IN ('PENDING', 'IN_PROGRESS');
+CREATE INDEX notification_outbox_polling_idx
+ON notification_outbox (next_retry_at ASC, id ASC)
+WHERE status = 'PENDING';
 --rollback DROP INDEX IF EXISTS notification_outbox_status_idx;
+
+--changeset quicklybly:BB-14-create-notification-outbox-stuck-idx
+CREATE INDEX notification_outbox_stuck_idx
+ON notification_outbox (updated_at)
+WHERE status = 'IN_PROGRESS';
+--rollback DROP INDEX IF EXISTS notification_outbox_stuck_idx;
 
 --changeset quicklybly:BB-14-create-notification-outbox-failed-idx
 CREATE INDEX notification_outbox_failed_idx
-ON notification_outbox (status, next_retry_at ASC)
+ON notification_outbox (next_retry_at ASC)
 WHERE status = 'FAILED';
 --rollback DROP INDEX IF EXISTS notification_outbox_failed_idx;
