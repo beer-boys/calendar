b3e41b5e74be19172eeb80d794d429f51718c443
a.k.lysenko <a.k.lysenko@tbank.ru>
2025-12-04 22:32:11 +0300

    chore: fix whitelist for /api/v1/me

--- src/main/kotlin/ru/itmo/dws/calendar/configuration/BasePath.kt
+++ src/main/kotlin/ru/itmo/dws/calendar/configuration/BasePath.kt
@@ -8,7 +8,7 @@ object BasePath {
     const val GOOGLE_OAUTH2 = "/oauth2/authorization/google"
     const val GOOGLE_CALLBACK = "/login/oauth2/code/google"
 
-    val WHITE_LIST = listOf(AUTH, READYZ)
+    val WHITE_LIST = listOf(AUTH, READYZ, "$BASE/me")
     val GOOGLE_WHITE_LIST = listOf(
         "$GOOGLE_BASE/**",
         "$GOOGLE_OAUTH2/**",
