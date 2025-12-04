5f474523ada8af2c9eb0cf7a6b845397f9a66bf7
Gogolev Andrey <nerud1502@gmail.com>
2025-12-05 00:51:47 +0300

    add configuration

---
+++ src/main/kotlin/ru/itmo/dws/calendar/configuration/CoreConfiguration.kt
@@ -0,0 +1,72 @@
+package ru.itmo.dws.calendar.configuration
+
+import java.time.ZoneId
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import ru.itmo.dws.calendar.core.port.input.HabitManagementUseCase
+import ru.itmo.dws.calendar.core.port.output.FocusTimeRepository
+import ru.itmo.dws.calendar.core.port.output.HabitRepository
+import ru.itmo.dws.calendar.core.port.output.MeetingRepository
+import ru.itmo.dws.calendar.core.service.ConflictDetectionService
+import ru.itmo.dws.calendar.core.service.HabitManagementService
+import ru.itmo.dws.calendar.core.service.HabitSlotFinder
+import ru.itmo.dws.calendar.core.service.provider.FocusTimeEventProvider
+import ru.itmo.dws.calendar.core.service.provider.HabitEventProvider
+import ru.itmo.dws.calendar.core.service.provider.MeetingEventProvider
+import ru.itmo.dws.calendar.core.service.provider.SchedulableEventProvider
+
+@Configuration
+class CoreConfiguration {
+
+    @Bean
+    fun habitSlotFinder(): HabitSlotFinder {
+        return HabitSlotFinder()
+    }
+
+    @Bean
+    fun habitEventProvider(habitRepository: HabitRepository): HabitEventProvider {
+        return HabitEventProvider(habitRepository)
+    }
+
+    @Bean
+    fun meetingEventProvider(meetingRepository: MeetingRepository): MeetingEventProvider {
+        return MeetingEventProvider(meetingRepository)
+    }
+
+    @Bean
+    fun focusTimeEventProvider(focusTimeRepository: FocusTimeRepository): FocusTimeEventProvider {
+        return FocusTimeEventProvider(focusTimeRepository)
+    }
+
+    @Bean
+    fun eventProviders(
+        habitEventProvider: HabitEventProvider,
+        meetingEventProvider: MeetingEventProvider,
+        focusTimeEventProvider: FocusTimeEventProvider
+    ): List<SchedulableEventProvider> {
+        return listOf(habitEventProvider, meetingEventProvider, focusTimeEventProvider)
+    }
+
+    @Bean
+    fun conflictDetectionService(
+        eventProviders: List<SchedulableEventProvider>
+    ): ConflictDetectionService {
+        return ConflictDetectionService(eventProviders)
+    }
+
+    @Bean
+    fun habitManagementUseCase(
+        habitRepository: HabitRepository,
+        eventProviders: List<SchedulableEventProvider>,
+        habitSlotFinder: HabitSlotFinder,
+        conflictDetectionService: ConflictDetectionService
+    ): HabitManagementUseCase {
+        return HabitManagementService(
+            habitRepository = habitRepository,
+            eventProviders = eventProviders,
+            habitSlotFinder = habitSlotFinder,
+            conflictDetectionService = conflictDetectionService,
+            zoneId = ZoneId.systemDefault()
+        )
+    }
+}
